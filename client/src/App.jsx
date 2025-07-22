0a7df85af0a72ab5075b7511e18c5ab27d5d6334
ShrutiJadhav14 <159283106+ShrutiJadhav14@users.noreply.github.com>
2025-07-22 18:47:19 +0530

    admin panel started , start with sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,11 @@ import Favourites from './pages/Favourites'
 import { Toaster} from 'react-hot-toast'
 import Footer from './components/Footer'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import  Dashboard from './pages/admin/Dashboard'
+import  AddShows from './pages/admin/AddShows'
+import  Layout  from './pages/admin/Layout'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 const App = () => {
   const isAdminRoute=useLocation().pathname.startsWith('/admin')
   return (
@@ -30,7 +35,13 @@ const App = () => {
         <Route path='/movies/:id'element={<MovieDetails/>}/>
         <Route path='/movies/:id/:date'element={<SeatLayout/>}/>
         <Route path='/my-bookings'element={<MyBookings/>}/>
-        <Route path='/favourites'element={<Favourites/>}/>        
+        <Route path='/favourites'element={<Favourites/>}/>   
+        <Route path="/admin" element={<Layout/>}>  
+        <Route index element={<Dashboard/>}/>
+        <Route path="add-shows" element={<AddShows/>}/>
+        <Route path="list-shows" element={<ListShows/>}/>
+        <Route path="list-bookings" element={<ListBookings/>}/>
+        </Route>  
       </Routes>
         {!isAdminRoute && <Footer/>}
     
